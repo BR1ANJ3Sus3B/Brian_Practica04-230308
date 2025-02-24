f14f8d3bced95887c4adf1d59b3d448ecfb36410
Brian Jesùs Mendoza Marquez <230308@utxicotepec.edu.mx>
2025-02-23 23:35:44 -0600

    Practica07
    Agregacion de ejercios

--- Briangular/src/app/app.component.ts
+++ Briangular/src/app/app.component.ts
@@ -3,10 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { Ejercicio01Component } from './ejercicio01/ejercicio01.component';
 import { Ejercicio02Component } from './ejercicio02/ejercicio02.component';
 import { Ejercicio03Component } from "./ejercicio03/ejercicio03.component";
+import { Ejercicio06Component } from './ejercicio06/ejercicio06.component';
+import { Ejercicio04Component } from './ejercicio04/ejercicio04.component';
+import { Ejercicio05Component } from './ejercicio05/ejercicio05.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Ejercicio01Component,Ejercicio02Component,Ejercicio03Component],
+  imports: [RouterOutlet, Ejercicio01Component,Ejercicio02Component,Ejercicio03Component,Ejercicio06Component,Ejercicio04Component,Ejercicio05Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ Briangular/src/app/ejercicio04/ejercicio04.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejercicio04',
+  imports: [],
+  templateUrl: './ejercicio04.component.html',
+  styleUrl: './ejercicio04.component.css'
+})
+export class Ejercicio04Component {
+  isServerRunning = true; 
+}
